756cf9dad8b2d27dffaf7b88e2bc9ed8908087c4
Tiffani Hess <hesstjune@gmail.com>
2020-11-03 16:56:05 -0600

    added some logic for creating schema from external api data

--- controllers/externalController.js
+++ controllers/externalController.js
@@ -1,13 +1,10 @@
 const axios = require("axios");
 
-exports.getData = (req, res) => {
-  var options = {
+exports.getData = async (req, res) => {
+  const options = {
     method: "GET",
     url: "https://rapidapi.p.rapidapi.com/",
     params: {
-      "q-activities_activity_type_name_eq": "null",
-      "q-city_cont": "null",
-      "q-state_cont": "null",
       "q-country_cont": "null",
       limit: "10",
     },
@@ -17,12 +14,59 @@ exports.getData = (req, res) => {
     },
   };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  const response = await axios.request(options);
+
+  const data = response.data;
+  const places = data.places;
+  let result = [];
+  for (let i = 0; i < places.length; i++) {
+    if (places[i].country !== "United States") {
+      continue;
+    }
+
+    if (places[i].activities.length < 1) {
+      continue;
+    }
+
+    const {
+      name,
+      country,
+      city,
+      lat,
+      lon,
+      description,
+      directions,
+      activities,
+    } = places[i];
+
+    const newSchema = {
+      name,
+      country,
+      city,
+      lat,
+      lon,
+      description,
+      directions,
+      activities,
+    };
+    result.push(newSchema);
+  }
+  // places.forEach((place) => {
+  //   //TODO: destructure activities for relevant data
+  //   if (place.country !== "United States") {
+  //   }
+  //   const { name, city, lat, lon, description, directions, activities } = place;
+  //   const newSchema = {
+  //     name,
+  //     city,
+  //     lat,
+  //     lon,
+  //     description,
+  //     directions,
+  //     activities,
+  //   };
+  //   result.push(newSchema);
+  // });
+
+  res.json(result);
 };
