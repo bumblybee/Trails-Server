7dea983f8793bec4dfe4400c51686e8bad092ab7
Tiffani Hess <hesstjune@gmail.com>
2020-11-02 19:42:15 -0600

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 const errorHandlers = require("./handlers/errorHandlers");
 
 var indexRouter = require("./routes/index");
@@ -14,6 +15,12 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
