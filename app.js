9fcdf20d539ec083854449f1761a9c5a2357ca1c
Tiffani Hess <hesstjune@gmail.com>
2020-11-02 19:39:28 -0600

    set up basic error handlers, removed jade

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const errorHandlers = require("./handlers/errorHandlers");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -22,18 +23,25 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+// });
+app.use(errorHandlers.notFound);
+
+if (app.get("env") === "development") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
 
 module.exports = app;
