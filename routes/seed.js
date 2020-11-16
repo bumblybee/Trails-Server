6fb475da1cb6ad00746d45de4dabf4e46c206138
Tiffani Hess <hesstjune@gmail.com>
2020-11-16 17:48:44 -0600

    wrap /combined route in errorWrapper

--- routes/seed.js
+++ routes/seed.js
@@ -5,7 +5,7 @@ const { errorWrapper } = require("../handlers/errorHandlers");
 
 //TODO: if leaving api calls attached to routes, verify auth to access
 
-router.get("/combined", seedController.getCombinedTrails);
+router.get("/combined", errorWrapper(seedController.getCombinedTrails));
 router.get("/biking", errorWrapper(seedController.getBikingByState));
 router.get("/hiking", errorWrapper(seedController.getHikingByState));
 
