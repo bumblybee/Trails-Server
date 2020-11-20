0231647d8890fb48693d9ed222a5d8f8478200cc
Tiffani Hess <hesstjune@gmail.com>
2020-11-20 13:57:49 -0600

    added grabAllHikingData axios.all call to dataGrabService and edited code for hiking in seedController

--- controllers/seedController.js
+++ controllers/seedController.js
@@ -3,8 +3,7 @@ const states = require("../enums/states");
 
 const {
   grabCombinedData,
-  grabBikingData,
-  grabHikingData,
+  grabAllHikingData,
   grabAllBikingData,
 } = require("../services/dataGrabService");
 
@@ -17,43 +16,68 @@ exports.getCombinedTrails = async (req, res) => {
 };
 
 exports.getBikingByState = async (req, res) => {
-  const trails = [];
-  // const kentuckyData = await grabBikingData(
+  const stateLatLng = [];
+
+  // pull lat and lng from each state object
+  states.map((state) => {
+    const entries = Object.values(state);
+
+    stateLatLng.push(...entries);
+  });
+
+  // const data = await grabAllBikingData(stateLatLng);
+  const trails = [...data];
+
+  // const storedTrails = await seedService.storeBikingTrailsInDb(trails);
+
+  res.json(trails);
+};
+
+exports.getHikingByState = async (req, res) => {
+  const stateLatLng = [];
+
+  // pull lat and lng from each state object
+  states.map((state) => {
+    const entries = Object.values(state);
+
+    stateLatLng.push(...entries);
+  });
+
+  const data = await grabAllHikingData(stateLatLng);
+  const trails = [...data];
+
+  // const storedTrails = await seedService.storeHikingTrailsInDb(trails);
+
+  res.json(trails);
+
+  // const trails = [];
+
+  // const kentuckyData = await grabHikingData(
   //   states.kentucky.lat,
   //   states.kentucky.lng
   // );
-  // const iowaData = await grabBikingData(states.iowa.lat, states.iowa.lng);
-  // const kansasData = await grabBikingData(states.kansas.lat, states.kansas.lng);
-  // const southDakotaData = await grabBikingData(
+  // const iowaData = await grabHikingData(states.iowa.lat, states.iowa.lng);
+  // const kansasData = await grabHikingData(states.kansas.lat, states.kansas.lng);
+  // const southDakotaData = await grabHikingData(
   //   states.southDakota.lat,
   //   states.southDakota.lng
   // );
-  // const louisianaData = await grabBikingData(
+  // const louisianaData = await grabHikingData(
   //   states.louisiana.lat,
   //   states.louisiana.lng
   // );
-  // const nebraskaData = await grabBikingData(
+  // const nebraskaData = await grabHikingData(
   //   states.nebraska.lat,
   //   states.nebraska.lng
   // );
-  // const mississippiData = await grabBikingData(
+  // const mississippiData = await grabHikingData(
   //   states.mississippi.lat,
   //   states.mississippi.lng
   // );
-  // const missouriData = await grabBikingData(
+  // const missouriData = await grabHikingData(
   //   states.missouri.lat,
   //   states.missouri.lng
   // );
-
-  states.map((state) => {
-    // const data = await grabBikingData(state.lat, state.lng);
-    // trails.push(...data);
-    const entries = Object.values(state);
-
-    trails.push(...entries);
-  });
-
-  // res.json((mapped = trails.map((trail) => trail)));
 
   // trails.push(
   //   ...kentuckyData,
@@ -66,58 +90,7 @@ exports.getBikingByState = async (req, res) => {
   //   ...missouriData
   // );
 
-  // Promise.all(trails).then(async (res) => {
-  //   const data = await res;
-  //   res.json(data);
-  // });
-
-  // const storedTrails = await seedService.storeBikingTrailsInDb(trails);
-  const data = await grabAllBikingData(trails);
-  res.json(data);
-};
-
-exports.getHikingByState = async (req, res) => {
-  const trails = [];
-
-  const kentuckyData = await grabHikingData(
-    states.kentucky.lat,
-    states.kentucky.lng
-  );
-  const iowaData = await grabHikingData(states.iowa.lat, states.iowa.lng);
-  const kansasData = await grabHikingData(states.kansas.lat, states.kansas.lng);
-  const southDakotaData = await grabHikingData(
-    states.southDakota.lat,
-    states.southDakota.lng
-  );
-  const louisianaData = await grabHikingData(
-    states.louisiana.lat,
-    states.louisiana.lng
-  );
-  const nebraskaData = await grabHikingData(
-    states.nebraska.lat,
-    states.nebraska.lng
-  );
-  const mississippiData = await grabHikingData(
-    states.mississippi.lat,
-    states.mississippi.lng
-  );
-  const missouriData = await grabHikingData(
-    states.missouri.lat,
-    states.missouri.lng
-  );
-
-  trails.push(
-    ...kentuckyData,
-    ...iowaData,
-    ...kansasData,
-    ...southDakotaData,
-    ...louisianaData,
-    ...nebraskaData,
-    ...mississippiData,
-    ...missouriData
-  );
-
   // const storedTrails = await seedService.storeHikingTrailsInDb(trails);
 
-  res.json(storedTrails);
+  // res.json(storedTrails);
 };
--- services/dataGrabService.js
+++ services/dataGrabService.js
@@ -27,27 +27,7 @@ exports.grabCombinedData = async () => {
   return trails;
 };
 
-exports.grabBikingData = async (lat, lng) => {
-  const res = await axios.request({
-    method: "GET",
-    url: "https://trailapi-trailapi.p.rapidapi.com/trails/explore/",
-    params: { lat: lat, lon: lng, radius: "100", per_page: "5" },
-    headers: {
-      "x-rapidapi-key": process.env.TRAILS_API_KEY,
-      "x-rapidapi-host": process.env.TRAILS_API_HOST,
-    },
-  });
-
-  if (res.error) {
-    console.log(res.error);
-  }
-
-  const trails = res.data.data;
-
-  return trails;
-};
-
-exports.grabHikingData = async (lat, lng) => {
+const grabHikingData = async (lat, lng) => {
   const res = await axios.request({
     method: "GET",
     url: "https://www.hikingproject.com/data/get-trails",
@@ -67,9 +47,37 @@ exports.grabHikingData = async (lat, lng) => {
   return trails;
 };
 
-exports.grabAllBikingData = async (trails) => {
-  const mappedStates = trails.map((state) =>
-    this.grabBikingData(state.lat, state.lng)
+exports.grabAllHikingData = async (stateLatLng) => {
+  const mappedStates = stateLatLng.map((state) =>
+    grabHikingData(state.lat, state.lng)
+  );
+  const res = await axios.all(mappedStates);
+  return res;
+};
+
+const grabBikingData = async (lat, lng) => {
+  const res = await axios.request({
+    method: "GET",
+    url: "https://trailapi-trailapi.p.rapidapi.com/trails/explore/",
+    params: { lat: lat, lon: lng, radius: "100", per_page: "5" },
+    headers: {
+      "x-rapidapi-key": process.env.TRAILS_API_KEY,
+      "x-rapidapi-host": process.env.TRAILS_API_HOST,
+    },
+  });
+
+  if (res.error) {
+    console.log(res.error);
+  }
+
+  const trails = res.data.data;
+
+  return trails;
+};
+
+exports.grabAllBikingData = async (stateLatLng) => {
+  const mappedStates = stateLatLng.map((state) =>
+    grabBikingData(state.lat, state.lng)
   );
   const res = await axios.all(mappedStates);
   return res;
